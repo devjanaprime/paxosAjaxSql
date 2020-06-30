43335686b4030abcb376d2b007d420768f54520d
Dev Jana <devjana@Devs-MBP-2.domain>
2020-06-30 11:52:45 -0500

    end of GET lecture

--- server/modules/birds.route.js
+++ server/modules/birds.route.js
@@ -1,10 +1,19 @@
 // requires
 const express = require( 'express' );
 const router = express.Router();
+const pool = require( './pool' );
 // routes
 router.get( '/', ( req, res )=>{
     console.log( '/birds GET' );
-    res.send( 'woof' );
+    /// - query: SELECT * FROM "birds" - ///
+    let queryString = `SELECT * FROM "birds"`;
+    pool.query( queryString ).then( ( result )=>{
+        // success
+        res.send( result.rows );
+    }).catch( ( err )=>{
+        // error
+        res.send( 500 );
+    })
 }) // end /birds GET
 // export
 module.exports = router;
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,23 @@
 $( document ).ready( onReady );
 
 function onReady(){
-    console.log( 'JQ' );
+    getBirds();
+}
+
+function getBirds(){
+    $.ajax({
+        type: 'GET',
+        url: '/birds'
+    }).then( function( response ){
+        console.log( 'back from GET:', response );
+        let el = $( '#birdsOut' );
+        el.empty();
+        for( let i=0; i< response.length; i++ ){
+            el.append( `<li>
+            ${ response[i].first_name }</li>`)
+        }
+    }).catch( function( err ){
+        console.log( err );
+        alert( 'nope' );
+    }) //end AJAX
 }
